3128e998937419b49313787c28b697c07acfc13f
Mehdi Laib <123883015+mehdilb8@users.noreply.github.com>
2024-03-21 09:12:36 +0100

    Update ajouter_evenement.php

--- Gestion/Activites/ajouter_evenement.php
+++ Gestion/Activites/ajouter_evenement.php
@@ -1,25 +1,25 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer le type d'événement depuis le formulaire
+    
     $type = $_POST["type"];
 
-    // Connexion à la base de données
+    
     $conn = new mysqli("127.0.0.1", "root", "", "manif");
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Gestion de l'ajout en fonction du type d'événement
+    
     if ($type === "responsable") {
-        // Récupérer les données du formulaire pour le responsable
+        
         $numResp = $_POST["num_resp"];
         $nomResp = $_POST["nom_resp"];
         $prenomResp = $_POST["prenom_resp"];
         
-        // Valider et sécuriser les données (ajoutez des validations supplémentaires au besoin)
+        
 
-        // Préparer la requête SQL pour insérer un nouveau responsable
+        
         $sql = "INSERT INTO responsable (num_resp,nom_resp, prenom_resp) VALUES (?,?, ?)";
         $stmt = $conn->prepare($sql);
 
@@ -37,14 +37,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Erreur lors de la préparation de la requête SQL.";
         }
     } elseif ($type === "activite") {
-        // Récupérer les données du formulaire pour l'activité
+        
         $nomAct = $_POST["nom_act"];
         $description = $_POST["description"];
         $numResp = $_POST["num_resp"];
 
-        // Valider et sécuriser les données (ajoutez des validations supplémentaires au besoin)
+        
 
-        // Préparer la requête SQL pour insérer une nouvelle activité
+        
         $sql = "INSERT INTO activite (nom_act, description, num_resp) VALUES (?, ?, ?)";
         $stmt = $conn->prepare($sql);
 
@@ -62,13 +62,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Erreur lors de la préparation de la requête SQL.";
         }
     } elseif ($type === "creneau") {
-        // Récupérer les données du formulaire pour le créneau
+        
         $heureDebut = $_POST["heure_debut"];
         $heureFin = $_POST["heure_fin"];
 
-        // Valider et sécuriser les données (ajoutez des validations supplémentaires au besoin)
+        
 
-        // Préparer la requête SQL pour insérer un nouveau créneau
+        
         $sql = "INSERT INTO creneau (heure_debut, heure_fin) VALUES (?, ?)";
         $stmt = $conn->prepare($sql);
 
@@ -91,7 +91,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $conn->close();
 } else {
-    // Redirection vers la page du formulaire si la requête n'est pas de type POST
+    
     header("Location: formulaire_evenements.php");
     exit();
 }
