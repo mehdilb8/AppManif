a2708ceb703dc192a4cd540fc194c49ae3aa9162
Mehdi Laib <123883015+mehdilb8@users.noreply.github.com>
2024-03-21 09:24:02 +0100

    Update modifier_responsable.php

--- corb/modifier_responsable.php
+++ corb/modifier_responsable.php
@@ -1,18 +1,18 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer les données du formulaire
+    
     $numResp = $_POST["responsable"]  ;
     $nouveauNom = $_POST["nouveau_nom"] ;
     $nouveauPrenom = $_POST["nouveau_prenom"] ;
 
-    // Connexion à la base de données
+
     $conn = new mysqli("127.0.0.1", "root", "", "manif");
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Vérifier si le responsable existe dans la base de données
+    
     $sql = "SELECT * FROM responsable WHERE num_resp = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $numResp);
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
-        // Le responsable existe, procédez à la mise à jour
+        
         $sql = "UPDATE responsable SET nom_resp = ?, prenom_resp = ? WHERE num_resp = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("ssi", $nouveauNom, $nouveauPrenom, $numResp);
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->close();
     $conn->close();
 } else {
-    // Redirection vers le formulaire de modification si la requête n'est pas de type POST
+    
     header("Location: modifier_responsable.php");
     exit();
 }
