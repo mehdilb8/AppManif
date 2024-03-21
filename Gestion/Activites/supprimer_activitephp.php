929fefb0f9634b90dcaa3af18254cb3b54db5f89
Mehdi Laib <123883015+mehdilb8@users.noreply.github.com>
2024-03-21 09:13:58 +0100

    Update supprimer_activitephp.php

--- Gestion/Activites/supprimer_activitephp.php
+++ Gestion/Activites/supprimer_activitephp.php
@@ -2,14 +2,14 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $idActivite = $_POST["activite"]; // Récupérer l'ID de l'activité à supprimer
 
-    // Connexion à la base de données
+    
     $conn = new mysqli("127.0.0.1", "root", "", "manif");
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Préparer la requête SQL pour supprimer l'activité
+    
     $sql = "DELETE FROM activite WHERE id_act = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $idActivite);
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->close();
     $conn->close();
 } else {
-    // Redirection vers le formulaire de suppression d'activité si la requête n'est pas de type POST
+    
     header("Location: supprimer_activite.php");
     exit();
 }
